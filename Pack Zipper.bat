354d2bcf2362e06c29f4afa0b1d4ff53ab9c1967
Diriector_Doc <davijust213@yahoo.ca>
2021-06-04 15:31:26 -0400

    Updated zipper

--- Pack Zipper.bat
+++ Pack Zipper.bat
@@ -1,6 +1,7 @@
 @echo off
 chcp 65001
 set name=pack
+set include=
 for /f "tokens=* delims=" %%f in (zipconfig.txt) do (
 	set %%f
 )
@@ -8,3 +9,4 @@ del "%name%.zip"
 "c:\Program Files\WinRAR\WinRAR.exe" a -afzip "%name%.zip" assets
 "c:\Program Files\WinRAR\WinRAR.exe" a -afzip "%name%.zip" pack.png
 "c:\Program Files\WinRAR\WinRAR.exe" a -afzip "%name%.zip" pack.mcmeta
+if not [%include%] == [] "c:\Program Files\WinRAR\WinRAR.exe" a -afzip "%name%.zip" %include%
